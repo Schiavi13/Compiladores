082152c273d7cdd84a2f66b9ae44e867ecd0d8f3
Luis Martínez <lufmartinezmu@gmail.com>
2019-05-01 17:27:38 -0500

    Add files via upload

--- analizadorLexicoPascal.py
+++ analizadorLexicoPascal.py
@@ -2,185 +2,125 @@ import ply.lex as lex
 import sys
 
 # list of tokens
-tokens = (
-    # Palabras reservadas
-    'AND',
-    'ARRAY',
-    'BEGIN',
-    'CASE',
-    'CONST',
-    'DIV',
-    'DO',
-    'DOWNTO',
-    'ELSE',
-    'END',
-    'FILE',
-    'FOR',
-    'FUNCTION',
-    'GOTO',
-    'IF',
-    'IN',
-    'LABEL',
-    'MOD',
-    'NIL',
-    'NOT',
-    'OF',
-    'OR',
-    'PACKED',
-    'PROCEDURE',
-    'PROGRAM',
-    'RECORD',
-    'REPEAT',
-    'SET',
-    'THEN',
-    'TO',
-    'TYPE',
-    'UNTIL',
-    'VAR',
-    'WHILE',
-    'WITH',
-
+tokens = [
     # Simbolos
-    'PLUS',
-    'MINUS',
+    'PLUS', #suma +
+    'MINUS', #resta -
     'TIMES', #por *
-    'DIVIDE',
-    'EQUAL',
-    'LESSER',
-    'GREATER',
+    'DIVIDE', #division /
+    'EQUAL', #igual =
+    'LESSER', #menor que <
+    'GREATER', #mayor que >
     'LBRACKET', #cuadrado [
     'RBRACKET', #cuadrado ]
-    'DOT',
-    'COMMA',
+    'DOT', #punto .
+    'COMMA', #coma ,
     'LPAREN', #parentesis (
     'RPAREN', #parentesis )
-    'COLON',
-    'CARAT', #elevado ^
-    'AT', #arroba @
+    'COLON', #dos puntos :
+    'SEMICOLON', #punto y coma ;
+    'POINTER', #se utiliza elevado ^
+    'AT', #arroba @ se utiliza para punteros
     'LBRACE', #llave {
     'RBRACE', #llave }
     'DOLLAR', #peso $
     'NUMSIGN', #numeral #
-    'AMPERSAND',
-    'PERCENT',
+    'AMPERSAND', #and per se and &
+    'PERCENT', #porcentaje %
+    'BACKQUOTE',#tilde invertida `
+    'WHITESPACE', #espacio en blanco
+    'TILDE', #virgulilla ~
+    'BAR', #barra |
+    'EXCLAMATION', #exclamacion !
+    'ISNOTEQ', #no es igual <>
+    'GREATEREQ', #mayor o igual >=
+    'LESSEREQ', #menor o igual <=
+    'LSHIFT', #binary left shift <<
+    'RSHIFT', #binart right shift >>
+    'SUBRANGE', #operador subrango doble punto ..
+    'ASSIGN', #se usa := para asignar valores
+    'QUOTE', #comilla '
 
     # otros
     'ID',
-    'NUMBER',
+    'NUMBER',   
+]
 
-    
-)
+# Segun la documentacion de ply es mejor definir las palabras reservadas aparte 
+reservadas = {'AND':'and',
+    'ARRAY':'array',
+    'BEGIN':'begin',
+    'CASE':'case',
+    'CONST':'const',
+    'DIV':'div',
+    'DO':'do',
+    'DOWNTO':'downto',
+    'ELSE':'else',
+    'END':'end',
+    'FILE':'file',
+    'FOR':'for',
+    'FUNCTION':'function',
+    'GOTO':'goto',
+    'IF':'if',
+    'IN':'in',
+    'LABEL':'label',
+    'MOD':'mod',
+    'NIL':'nil',
+    'NOT':'not',
+    'OF':'of',
+    'OR':'or',
+    'PACKED':'packed',
+    'PROCEDURE':'procedure',
+    'PROGRAM':'program',
+    'RECORD':'record',
+    'REPEAT':'repeat',
+    'SET':'set',
+    'THEN':'then',
+    'TO':'to',
+    'TYPE':'type',
+    'UNTIL':'until',
+    'VAR':'var',
+    'WHILE':'while',
+    'WITH':'with',
+    }
 
-# Regular expressions rules for a simple tokens 
+# se agregan las palabras reservadas a la lista de tokens
+tokens = tokens+list(reservadas.values())
+
+t_ignore = '\t'
+# Expresiones regulares para tokens atomicos
 t_PLUS   = r'\+'
 t_MINUS  = r'-'
 t_TIMES  = r'\*'
 t_DIVIDE = r'/'
 t_EQUAL  = r'='
-t_DISTINT = r'!'
-t_LESS   = r'<'
+t_LESSER   = r'<'
 t_GREATER = r'>'
-t_SEMICOLON = ';'
-t_COMMA  = r','
-t_LPAREN = r'\('
-t_RPAREN  = r'\)'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
-t_LBLOCK   = r'{'
-t_RBLOCK   = r'}'
-t_COLON   = r':'
-t_AMPERSANT = r'\&'
-t_HASHTAG = r'\#'
 t_DOT = r'\.'
-
-def t_AUTO(t):
-    r'auto'
-    return t
-
-def t_BREAK(t):
-    r'break'
-    return t
-
-def t_CASE(t):
-    r'case'
-    return t
-
-def t_CHAR(t):
-    r'char'
-    return t
-
-def t_BOOLEAN(t):
-    r'boolean'
-    return t
-
-def t_INCLUDE(t):
-    r'include'
-    return t
+t_COMMA  = r',' 
+t_LPAREN = r'\('
+t_RPAREN  = r'\)'
+t_COLON   = r':'
+t_SEMICOLON = r';'
+t_POINTER = r'\^'
+t_AT = r'@'
+t_LBRACE   = r'\{'
+t_RBRACE   = r'\}'
+t_DOLLAR = r'\$'
+t_NUMSIGN = r'\#'
+t_AMPERSAND = r'&'
+t_PERCENT = r'%'
+t_BACKQUOTE = r'`'
+t_WHITESPACE = r'\s+'
+t_TILDE = r'~'
+t_BAR = r'\|'
+t_EXCLAMATION = r'!'
+t_QUOTE = r'\''
 
 
-def t_DEFINE(t):
-    r'define'
-    return t
 
-def t_CONTINUE(t):
-    r'continue'
-    return t
-
-def t_DEFAULT(t):
-    r'default'
-    return t
-
-def t_DO(t):
-    r'do'
-    return t
-
-def t_DOUBLE(t):
-    r'double'
-    return t
-
-def t_ELSE(t):
-    r'else'
-    return t
-
-def t_FLOAT(t):
-    r'float'
-    return t
-
-def t_FOR(t):
-    r'for'
-    return t
-
-def t_IF(t):
-    r'if'
-    return t
-
-def t_INT(t):
-	r'int'
-	return t
-
-def t_SHORT(t):
-    r'short'
-    return t
-
-def t_LONG(t):
-    r'long'
-    return t
-
-def t_RETURN(t):
-	r'return'
-	return t
-
-def t_SWITCH(t):
-        r'switch'
-        return t
-
-def t_VOID(t):
-	r'void'
-	return t
-	
-def t_WHILE(t):
-	r'while'
-	return t
 
 def t_NUMBER(t):
     r'\d+(\.\d+)?'
@@ -188,51 +128,61 @@ def t_NUMBER(t):
     return t
 
 def t_ID(t):
-    r'\w+(_\d\w)*'
+    r'[a-zA-Z_][a-zA-Z_d]*'
     return t
 
-def t_LESSEQUAL(t):
-	r'<='
-	return t
+def t_ISNOTEQ(t):
+    r'<>'
+    return t
 
-def t_GREATEREQUAL(t):
+def t_GREATEREQ(t):
 	r'>='
 	return t
 
-def t_DEQUAL(t):
-	r'!='
+def t_LESSEREQ(t):
+	r'<='
 	return t
 
-def t_ISEQUAL(t):
-	r'=='
-	return t
-    
-def t_MINUSMINUS(t):
-	r'--'
-	return t
+def t_LSHIFT(t):
+    r'<<'
+    return t
 
-def t_PLUSPLUS(t):
-	r'\+\+'
-	return t
+def t_RSHIFT(t):
+    r'>>'
+    return t
+
+def t_SUBRANGE(t):
+    r'\.\.'
+    return t
+
+def t_ASSIGN(t):
+    r':='
+    return t
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-t_ignore = ' \t'
 
-def t_comments(t):
-    r'/\*(.|\n)*?\*/'
+def t_comentario_multi(t):
+    r'\(\*(.|\n)*?\*\) | \{(.|\n)*?\}'
     t.lexer.lineno += t.value.count('\n')
 
-def t_comments_C99(t):
+def t_comentario_simple(t):
     r'//(.)*?\n'
     t.lexer.lineno += 1
+
+def t_cadena(t):
+    r"\'[^']\'"
+    pass
 
 def t_error(t):
     print ("Lexical error: " + str(t.value[0]))
     t.lexer.skip(1)
-    
+
+
+
+
 def test(data, lexer):
 	lexer.input(data)
 	while True:
@@ -248,7 +198,7 @@ if __name__ == '__main__':
 	if (len(sys.argv) > 1):
 		fin = sys.argv[1]
 	else:
-		fin = 'evaluacion.c'
+		fin = 'holamundo.pas'
 	f = open(fin, 'r')
 	data = f.read()
 	print (data)
